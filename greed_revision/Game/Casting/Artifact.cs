d02d0a4643829a5988cb75208466f70b14a428a5
The-Marquis <82724294+The-Marquis@users.noreply.github.com>
2022-07-13 14:48:46 -0600

    Add files via upload

---
+++ greed_revision/Game/Casting/Artifact.cs
@@ -0,0 +1,25 @@
+namespace greed_revision.Game.Casting
+{
+    // TODO: Implement the Artifact class here
+
+    // 1) Add the class declaration. Use the following class comment. Make sure you
+    //    inherit from the Actor class.
+    public class Artifact : Actor{
+        /// <summary>
+        /// <para>An item of cultural or historical interest.</para>
+        /// <para>
+        /// The responsibility of an Artifact is to provide a message about itself.
+        /// </para>
+        /// </summary>
+        private string itemName;
+
+
+    // 2) Create the class constructor. Use the following method comment.
+        public Artifact(){
+
+        }
+        /// <summary>
+        /// Constructs a new instance of Artifact.
+        /// </summary>
+    }
+}
---
+++ greed_revision/Game/Directing/Director.cs
@@ -0,0 +1,98 @@
+using System.Collections.Generic;
+using greed_revision.Game.Casting;
+using greed_revision.Game.Services;
+
+
+namespace greed_revision.Game.Directing
+{
+    /// <summary>
+    /// <para>A person who directs the game.</para>
+    /// <para>
+    /// The responsibility of a Director is to control the sequence of play.
+    /// </para>
+    /// </summary>
+    public class Director
+    {
+        private KeyboardService keyboardService = null;
+        private VideoService videoService = null;
+
+        /// <summary>
+        /// Constructs a new instance of Director using the given KeyboardService and VideoService.
+        /// </summary>
+        /// <param name="keyboardService">The given KeyboardService.</param>
+        /// <param name="videoService">The given VideoService.</param>
+        public Director(KeyboardService keyboardService, VideoService videoService)
+        {
+            this.keyboardService = keyboardService;
+            this.videoService = videoService;
+        }
+
+        /// <summary>
+        /// Starts the game by running the main game loop for the given cast.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        public void StartGame(Cast cast)
+        {
+            videoService.OpenWindow();
+            while (videoService.IsWindowOpen())
+            {
+                GetInputs(cast);
+                DoUpdates(cast);
+                DoOutputs(cast);
+            }
+            videoService.CloseWindow();
+        }
+
+        /// <summary>
+        /// Gets directional input from the keyboard and applies it to the robot.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        private void GetInputs(Cast cast)
+        {
+            Actor robot = cast.GetFirstActor("player");
+            Point velocity = keyboardService.GetDirection();
+            robot.SetVelocity(velocity);     
+        }
+
+        /// <summary>
+        /// Updates the robot's position and resolves any collisions with artifacts.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        private void DoUpdates(Cast cast)
+        {
+            Actor banner = cast.GetFirstActor("banner");
+            Actor player = cast.GetFirstActor("player");
+            List<Actor> artifacts = cast.GetActors("artifacts");
+
+            banner.SetText($"Score : {player.GetValue()}");
+            int maxX = videoService.GetWidth();
+            int maxY = videoService.GetHeight();
+            player.MoveNext(maxX, maxY, "player");
+
+            foreach (Actor actor in artifacts)
+            {
+                if (player.GetPosition().Equals(actor.GetPosition()))
+                {   
+                    int robotValue = player.GetValue();
+                    player.SetValue(robotValue += actor.GetValue());
+                    cast.RemoveActor("artifacts", actor);
+                    
+                }
+                actor.MoveNext(maxX, maxY, "artifacts");
+            } 
+        }
+
+        /// <summary>
+        /// Draws the actors on the screen.
+        /// </summary>
+        /// <param name="cast">The given cast.</param>
+        public void DoOutputs(Cast cast)
+        {
+            List<Actor> actors = cast.GetAllActors();
+            videoService.ClearBuffer();
+            videoService.DrawActors(actors);
+            videoService.FlushBuffer();
+        }
+
+    }
+}
